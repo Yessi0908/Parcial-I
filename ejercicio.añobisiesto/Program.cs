6027f76067adaed22d48b12ea5b37410bc3c73b1
Yessi0908 <jramirezs26@miumg.edu.gt>
2023-03-04 12:36:33 -0600

    ejercicio.añobisiesto

---
+++ ejercicio.añobisiesto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Programa01examen
+{class Program
+    {public static bool Añobisiesto(int a)
+        {if (a % 4 == 0 & (a % 100 != 0 | a % 400 == 0))
+         return true;
+         return false;
+        }
+        static void Main(string[] args)
+        {int j;
+            Console.Write("Escriba un año: ");
+            j = Int32.Parse(Console.ReadLine());
+            if (Añobisiesto(j) == true)
+            {  Console.WriteLine("{0} Es un año bisiesto", j);
+            }
+            else
+            {Console.WriteLine("{0} No es un año bisiesto", j);
+            }
+        }
+    }
+}
